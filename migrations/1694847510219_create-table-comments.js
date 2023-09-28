878ec4983eebdcf8a5500f3070d0d251c60f9bd5
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2023-09-28 14:56:12 +0800

    Fix eslint error.

--- migrations/1694847510219_create-table-comments.js
+++ migrations/1694847510219_create-table-comments.js
@@ -1,39 +1,39 @@
 /* eslint-disable camelcase */
 exports.up = (pgm) => {
-    pgm.createTable('comments', {
-        id: {
-            type: 'CHAR(29)',
-            primaryKey: true,
-        },
-        content: {
-            type: 'TEXT',
-            notNull: true,
-        },
-        owner: {
-            type: 'CHAR(26)',
-            notNull: true,
-            references: 'users(id)',
-            onDelete: 'cascade',
-        },
-        thread_id: {
-            type: 'CHAR(28)',
-            notNull: true,
-            references: 'threads(id)',
-            onDelete: 'cascade',
-        },
-        date: {
-            type: 'TIMESTAMPTZ',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
-        is_deleted: {
-            type: 'BOOLEAN',
-            notNull: true,
-            default: false,
-        },
-    });
-};
+  pgm.createTable('comments', {
+    id: {
+      type: 'CHAR(29)',
+      primaryKey: true
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true
+    },
+    owner: {
+      type: 'CHAR(26)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'cascade'
+    },
+    thread_id: {
+      type: 'CHAR(28)',
+      notNull: true,
+      references: 'threads(id)',
+      onDelete: 'cascade'
+    },
+    date: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    is_deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false
+    }
+  })
+}
 
 exports.down = (pgm) => {
-    pgm.dropTable('comments');
-};
+  pgm.dropTable('comments')
+}
--- migrations/1694847525199_create-table-replies.js
+++ migrations/1694847525199_create-table-replies.js
@@ -1,39 +1,39 @@
 /* eslint-disable camelcase */
 exports.up = (pgm) => {
-    pgm.createTable('replies', {
-        id: {
-            type: 'CHAR(27)',
-            primaryKey: true,
-        },
-        content: {
-            type: 'TEXT',
-            notNull: true,
-        },
-        owner: {
-            type: 'CHAR(26)',
-            references: 'users(id)',
-            onDelete: 'cascade',
-            notNull: true,
-        },
-        comment_id: {
-            type: 'CHAR(29)',
-            references: 'comments(id)',
-            onDelete: 'cascade',
-            notNull: true,
-        },
-        date: {
-            type: 'TIMESTAMPTZ',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
-        is_deleted: {
-            type: 'BOOLEAN',
-            notNull: true,
-            default: false,
-        },
-    });
-};
+  pgm.createTable('replies', {
+    id: {
+      type: 'CHAR(27)',
+      primaryKey: true
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true
+    },
+    owner: {
+      type: 'CHAR(26)',
+      references: 'users(id)',
+      onDelete: 'cascade',
+      notNull: true
+    },
+    comment_id: {
+      type: 'CHAR(29)',
+      references: 'comments(id)',
+      onDelete: 'cascade',
+      notNull: true
+    },
+    date: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    is_deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false
+    }
+  })
+}
 
 exports.down = (pgm) => {
-    pgm.dropTable('replies');
-};
+  pgm.dropTable('replies')
+}
