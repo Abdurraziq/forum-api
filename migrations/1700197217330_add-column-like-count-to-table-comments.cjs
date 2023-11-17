4b6ba1147bff3f52036f40366744272204afbcb9
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2023-11-17 13:08:39 +0800

    Add column like_count to table comments.

---
+++ migrations/1700197217330_add-column-like-count-to-table-comments.cjs
@@ -0,0 +1,13 @@
+exports.up = (pgm) => {
+  pgm.addColumn('comments', {
+    like_count: {
+      type: 'INTEGER',
+      notNull: true,
+      default: 0
+    }
+  })
+}
+
+exports.down = (pgm) => {
+  pgm.dropColumn('comments', 'like_count')
+}
