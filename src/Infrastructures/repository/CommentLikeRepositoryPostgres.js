154f2d67b68fdb7d6c79199aa735513bf0636969
Abdurraziq Bachmid <a.raziq.1992@outlook.co.id>
2023-11-27 07:27:46 +0800

    Revert "Add comment like implementation,"

--- src/Infrastructures/repository/CommentLikeRepositoryPostgres.js
+++ src/Infrastructures/repository/CommentLikeRepositoryPostgres.js
@@ -9,32 +9,4 @@ export default class CommentLikeRepositoryPostgres extends CommentLikeRepository
     this.#pool = pool
     this.#idGenerator = idGenerator
   }
-
-  async verifyUserIsLikedTheComment ({ userId, commentId }) {
-    const query = {
-      text: `SELECT id
-             FROM comment_likes
-             WHERE user_id = $1 AND comment_id = $2`,
-      values: [userId, commentId]
-    }
-    const { rowCount } = await this.#pool.query(query)
-    return rowCount !== 0
-  }
-
-  async addUserToLikedTheComment ({ userId, commentId }) {
-    const id = `like-${this.#idGenerator(20)}`
-    const query = {
-      text: 'INSERT INTO comment_likes VALUES($1, $2, $3)',
-      values: [id, userId, commentId]
-    }
-    await this.#pool.query(query)
-  }
-
-  async removeUserFromLikedTheComment ({ userId, commentId }) {
-    const query = {
-      text: 'DELETE FROM comment_likes WHERE user_id = $1 AND comment_id = $2',
-      values: [userId, commentId]
-    }
-    await this.#pool.query(query)
-  }
 }
